70465787509e17f0faeace88c55e746d1111752d
Kamil Pinkowski <kamil.pinkowski@unity.software>
2020-05-04 15:28:43 +0200

    Add Supplier Menu

---
+++ src/Menu/SupplierMenuBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Menu;
+
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+class SupplierMenuBuilder
+{
+    public function buildMenu(MenuBuilderEvent $menuBuilderEvent)
+    {
+        $menu = $menuBuilderEvent->getMenu();
+        $catalogMenu = $menu->getChild('catalog');
+
+        $catalogMenu
+            ->addChild('supplier', [
+                'route' => 'app_admin_supplier_index',
+            ])
+            ->setLabel('app.ui.suppliers')
+            ->setLabelAttribute('icon', 'file')
+        ;
+    }
+}
