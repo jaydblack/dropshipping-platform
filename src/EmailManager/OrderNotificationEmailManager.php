6d108b28abb200440cea2b73dde79ca4a0664ed1
Kamil Pinkowski <kamil.pinkowski@unity.software>
2020-05-04 20:35:31 +0200

    Add custom state machine flow for order notification email

---
+++ src/EmailManager/OrderNotificationEmailManager.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EmailManager;
+
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Mailer\Sender\SenderInterface;
+
+class OrderNotificationEmailManager
+{
+    /** @var SenderInterface */
+    private $sender;
+
+    /** @var ChannelContextInterface */
+    private $channelContext;
+
+    public function __construct(
+        SenderInterface $sender,
+        ChannelContextInterface $channelContext
+    ) {
+        $this->sender = $sender;
+        $this->channelContext = $channelContext;
+    }
+
+    public function notify(OrderInterface $order): void
+    {
+        /** @var ChannelInterface $channel */
+        $channel = $this->channelContext->getChannel();
+        $this->sender->send(
+            'order_notification',
+            ['email@example.com'],
+            ['order' => $order, 'channel' => $channel]
+        );
+    }
+}
